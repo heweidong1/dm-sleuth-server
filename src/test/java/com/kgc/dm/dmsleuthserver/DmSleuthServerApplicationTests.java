b7aa22c498266b5351172f2805b7ed6cef0a7039
heweidong1 <a18233117759@163.com>
2019-12-05 16:25:43 +0800

    Initial commit

---
+++ src/test/java/com/kgc/dm/dmsleuthserver/DmSleuthServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kgc.dm.dmsleuthserver;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmSleuthServerApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
